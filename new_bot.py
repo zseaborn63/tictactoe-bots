a931aceea5835ad070d66086a2087e255550c972
zseaborn63 <zseaobnr63@gmail.com>
2015-09-25 15:09:54 -0400

    Reworked win and block condition logic to be simpler

--- new_bot.py
+++ new_bot.py
@@ -43,36 +43,67 @@ class LogicBot(RandomBot):
         occur_matrix.append(diag_occur)
         return occur_matrix
 
-    def find_wins(self):
+    def find_row_wins(self):
         my_win = self.get_my_occurance()
         row = my_win[0]
-        col = my_win[1]
-        diag = my_win[2]
         for idx in range(len(row)):
             if row[idx] == 2:
-                return True
-            elif col[idx] == 2:
-                return True
-            else:
-                for idx in range(len(diag)):
-                    if diag[idx] == 2:
-                        return True
-                    else:
-                        return False
+                return True, idx
 
-    def find_blocks(self):
-            my_block = self.get_opponent_occurance()
-            row = my_block[0]
-            col = my_block[1]
-            diag = my_block[2]
+    def find_col_wins(self):
+        my_win = self.get_my_occurance()
+        col = my_win[1]
+        for idx in range(len(col)):
+            if col[idx] == 2:
+                return True, idx
+
+    def find_diag_wins(self):
+        my_win = self.get_my_occurance()
+        diag = my_win[2]
+            if col[idx] == 2:
+                return True
+
+    def find_row_blocks(self):
+        my_block = self.get_opponent_occurance()
+        row = my_block[0]
+        for idx in range(len(row)):
+            if row[idx] == 2:
+                return True, idx
+
+    def find_col_blocks(self):
+        my_block = self.get_opponent_occurance()
+        col = my_block[1]
+        if col[idx] == 2:
+            return True
+            else:
+
+    def find_diag_blocks(self):
+        my_block = self.get_opponent_occurance()
+        diag = my_block[2]
+        for idx in range(len(diag)):
+            if diag[idx] == 2:
+                return True
+
+    def play_win(self):
+        win_possible = self.find_wins()
+        if win_possible == True:
+            win = self.get_my_occurance()
+            row = win[0]
+            col = win[1]
+            diag = win[2]
             for idx in range(len(row)):
                 if row[idx] == 2:
-                    return True
-                elif col[idx] == 2:
-                    return True
-                else:
-                    for idx in range(len(diag)):
-                        if diag[idx] == 2:
-                            return True
-                        else:
-                            return False
+                    win_row = self.board[idx]
+                    for i in range(len(win_row)):
+                        if "_" not in win_row:
+                            if self.me not in win_row:
+                                print("{} {}".format(idx, i))
+
+                if col[idx] == 2:
+        pass
+
+    def play_block(self):
+        pass
+
+    def make_play(self):
+        pass
